b816be0783fe068dce01d63bbbe3436775787c10
Prateek Mishra <pmishra@mdsol.com>
2022-07-18 15:48:13 +0530

    promiss and fetch

---
+++ basic/promiss.js
@@ -0,0 +1,35 @@
+// //
+// //
+// //
+// //
+
+// // const getUsers = new Promise((resolve, reject) => {
+// //   resolve("It's Done");
+// //   //   reject("Error");
+// // });
+
+// // getUsers.then(
+// //   (res) => {
+// //     console.log("Resolve", res);
+// //   },
+// //   (rej) => {
+// //     console.log("Reject");
+// //   }
+// // );
+
+// const findReminder = (v1, v2) => {
+//   return new Promise((res, rej) => {
+//     if (v1 % v2 === 0) {
+//       rej("No Reminder");
+//     } else res(v1 % v2);
+//   });
+// };
+
+// findReminder(4, 1).then(
+//   (res) => {
+//     console.log(res);
+//   },
+//   (rej) => {
+//     console.log(rej);
+//   }
+// );
