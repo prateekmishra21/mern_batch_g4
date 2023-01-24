7bb152cf00c7ae97dd2fe8860381a5d409d49330
RkGreenPhire <pmishra@mdsol.com>
2023-01-24 10:07:53 +0530

    update

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.use(bodyParser.json());
 app.use("/auth", authRouter);
 app.use("/blog", blogRouter);
 
+app.get("", (req, res) => {
+  return res.send(
+    "<center><h3>Hello All. This is Node Application</h3></center>"
+  );
+});
+
 app.listen(4000, () => {
   console.log("SERVER STARTED ON PORT 4000");
 });
